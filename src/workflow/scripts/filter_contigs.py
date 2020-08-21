c3c7eea983483992a2bdbc447359df222c5a0e7a
Marco Kreuzer <marco.kreuzer@bioinformatics.unibe.ch>
2020-08-21 15:49:40 +0200

    config changed to optimal RAM usage

--- src/workflow/scripts/filter_contigs.py
+++ src/workflow/scripts/filter_contigs.py
@@ -4,11 +4,10 @@ import fire
 
 def filter_fasta(input_fasta, sample_name, min_length):
     for record in SeqIO.parse(input_fasta, "fasta"):
-        if len(record) >= min_length:
-            new_name= record.name.replace("NODE", f'{sample_name}')
-            new_name = new_name.replace("length", "l").replace("cov", "c")
-            print(">" + new_name)
-            print(record.seq)
+        if len(record) <= min_length:
+            record.id = record.name.replace("NODE", f'{sample_name}' + "_scf")\
+                .replace("_length", " length").replace("scf_", "scf")
+            print(record.format("fasta"), end="")
 
 if __name__ == '__main__':
     fire.Fire(filter_fasta)
