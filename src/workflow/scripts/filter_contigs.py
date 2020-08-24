e9d9aa6e7f36d9cd68c953a9b31335cabc50e285
Marco Kreuzer <marco.kreuzer@bioinformatics.unibe.ch>
2020-08-24 15:42:34 +0200

    bug in filter_contigs.py corrected

--- src/workflow/scripts/filter_contigs.py
+++ src/workflow/scripts/filter_contigs.py
@@ -4,7 +4,7 @@ import fire
 
 def filter_fasta(input_fasta, sample_name, min_length):
     for record in SeqIO.parse(input_fasta, "fasta"):
-        if len(record) <= min_length:
+        if len(record) >= min_length:
             record.id = record.name.replace("NODE", f'{sample_name}' + "_scf")\
                 .replace("_length", " length").replace("scf_", "scf")
             print(record.format("fasta"), end="")
